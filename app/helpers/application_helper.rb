e46810a226dd10cdc02aeed394109624f870795f
sam <simofreelancer@yahoo.com>
2012-09-24 11:49:16 +0300

    finish user signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	#returns the full title on a per-page basis
 	def full_title(page_title)
-		base_title = "ruby on rails tutorial sample app"
+		base_title = "tripple"
 		if page_title.empty?
 			base_title
 		else
---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+	factory :user do
+		name	"peter han"
+		email "peter@example.com"
+		password "peter"
+		password_confimation "peter"
+		University "jkuat"
+		Highschool "mosocho"
+	end
+end	
